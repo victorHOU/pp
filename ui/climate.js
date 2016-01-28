0ead54efc757628e2d533f5bc653f4885224cee8
victorHOU <noreply>
2016-01-28 12:34:05 +0000

    Add module Climate

---
+++ ui/climate.js
@@ -0,0 +1,15 @@
+/**
+ * @module ui/climate
+ */
+var Montage = require("montage/core/core").Montage;
+/**
+ * @class Climate
+ * @extends Montage
+ */
+exports.Climate = Montage.specialize(/** @lends Climate# */ {
+    constructor: {
+        value: function Climate() {
+            this.super();
+        }
+    }
+});
